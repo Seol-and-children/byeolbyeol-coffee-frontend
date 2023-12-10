c2ab72a9f6d753c2ddb7e36b0c9d92cb77b7988e
phj9745 <1022phj@naver.com>
2023-12-10 16:17:11 +0900

    좋아요기능, 레시피 상세보기 페이지 수정

--- src/pages/RecipeDetailViewPage.js
+++ src/pages/RecipeDetailViewPage.js
@@ -2,11 +2,18 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
 import "./RecipeDetailViewPage.css";
+import LikeButton from "../components/recipe/LikeButton";
 
 function RecipeDetailViewPage() {
   const { recipeId } = useParams();
   const [recipe, setRecipe] = useState(null);
   const navigate = useNavigate();
+  const [isLiked, setIsLiked] = useState(false);
+
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
 
   useEffect(() => {
     const fetchRecipeDetails = async () => {
@@ -15,8 +22,17 @@ function RecipeDetailViewPage() {
           `http://localhost:8080/recipes/${recipeId}`
         );
         setRecipe(response.data);
+
+        // 좋아요 상태 확인 요청 추가
+        const likeStatusResponse = await axios.get(
+          `http://localhost:8080/recipes/${recipeId}/likes/status`,
+          {
+            params: { userId: 4 }, // 현재 로그인한 사용자 ID
+          }
+        );
+        setIsLiked(likeStatusResponse.data);
       } catch (error) {
-        console.error("레시피 상세 정보를 가져오는데 실패했습니다:", error);
+        console.error("데이터 로딩 중 오류 발생:", error);
       }
     };
 
@@ -26,9 +42,26 @@ function RecipeDetailViewPage() {
   const handleDelete = async () => {
     try {
       await axios.delete(`http://localhost:8080/recipes/${recipeId}`);
-      navigate("/recipes"); // 삭제 후 홈이나 레시피 목록 페이지로 리다이렉트
+      navigate("/recipes");
     } catch (error) {
       console.error("레시피 삭제 실패:", error);
+    }
+  };
+
+  const toggleLike = async () => {
+    try {
+      await axios.post(`http://localhost:8080/recipes/${recipeId}/likes`, {
+        userId: 4,
+      });
+      setIsLiked(!isLiked);
+      setRecipe((prevRecipe) => ({
+        ...prevRecipe,
+        likesCount: isLiked
+          ? prevRecipe.likesCount - 1
+          : prevRecipe.likesCount + 1,
+      }));
+    } catch (error) {
+      console.error("레시피 좋아요 실패:", error);
     }
   };
 
@@ -38,20 +71,40 @@ function RecipeDetailViewPage() {
 
   return (
     <div className="recipe-detail-view-page">
+      <p>{recipe.userNickname}</p>
+      <p>{formatDate(recipe.registerTime)}</p>
       <h2>{recipe.recipeName}</h2>
+      <button onClick={handleDelete}>삭제</button>
       <img
         src={`http://localhost:8080/recipeimgs/${recipe.photoUrl}`}
         alt={recipe.recipeName}
       />
       <p>프랜차이즈: {recipe.franchiseName}</p>
-      <p>베이스 음료: {recipe.baseBeverageVO.name}</p>
-      <p>커스텀 옵션: {recipe.customOptionId}</p>
+      <p>
+        베이스 음료: {recipe.baseBeverageVO.name} {recipe.baseBeverageVO.size}{" "}
+        {recipe.baseBeverageVO.temperature}
+      </p>
+      <div>
+        <p>커스텀 옵션</p>
+        <ul>
+          {recipe.customOptions.map((option, index) => (
+            <li key={index}>
+              {option.customOptionId ? (
+                <span>
+                  {option.ingredientName} --- {option.quantity}
+                  {option.ingredientUnit}
+                </span>
+              ) : (
+                <span>옵션 정보를 찾을 수 없음</span>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
       <p>설명: {recipe.description}</p>
-      <button onClick={handleDelete}>레시피 삭제</button>
-      <button onClick={() => navigate("/recipes")}>
-        레시피 목록으로 돌아가기
-      </button>
-      {/* 필요에 따라 추가 버튼 및 기능을 추가하세요 */}
+      <button onClick={() => navigate("/recipes")}>목록</button>
+      <LikeButton isLiked={isLiked} toggleLike={toggleLike} />
+      <p>추천수: {recipe.likesCount}</p>
     </div>
   );
 }
