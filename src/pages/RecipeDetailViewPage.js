3ab05d4e3aab5e9bcc0b5e1a5ce3360cd9dadd44
phj9745 <1022phj@naver.com>
2023-12-16 02:07:59 +0900

    카페 별 모아보기 디자인, 프랜차이즈 로고 불러오기 수정 등

--- src/pages/RecipeDetailViewPage.js
+++ src/pages/RecipeDetailViewPage.js
@@ -13,6 +13,7 @@ function RecipeDetailViewPage() {
   const [isLiked, setIsLiked] = useState(false);
   const user = useSelector((state) => state.user.userData);
   const userId = user ? user.userId : null;
+  const userRole = user ? user.userRole : null;
 
   const formatDate = (dateString) => {
     const options = { year: "numeric", month: "long", day: "numeric" };
@@ -88,24 +89,34 @@ function RecipeDetailViewPage() {
     }
   };
 
+  const isAuthor = recipe && userId && recipe.authorId === userId;
+
   if (!recipe) {
-    return <div>로딩 중...</div>;
+    return <div>레시피 불러오는중...</div>;
   }
 
   return (
     <div className="recipe-detail-view-page">
       <div className="header">
         <h1>RECIPE</h1>
-        {userId && recipe.authorId === userId && (
-          <div className="buttons">
-            <button className="edit-button" onClick={navigateToEdit}>
-              수정
-            </button>
+        <div className="buttons">
+          {userRole === 3 && (
             <button className="delete-button" onClick={handleDelete}>
               삭제
             </button>
-          </div>
-        )}
+          )}
+
+          {userRole === 2 && isAuthor && (
+            <div>
+              <button className="edit-button" onClick={navigateToEdit}>
+                수정
+              </button>
+              <button className="delete-button" onClick={handleDelete}>
+                삭제
+              </button>
+            </div>
+          )}
+        </div>
       </div>
 
       <div className="author-info">
@@ -154,9 +165,11 @@ function RecipeDetailViewPage() {
           </div>
           <div className="description">{recipe.description}</div>
         </div>
-        <div className="report-button">
-          <button>신고하기</button>
-        </div>
+        {userRole === 2 && !isAuthor && (
+          <div className="report-button">
+            <button>신고하기</button>
+          </div>
+        )}
         <div className="like-button">
           <div className="like-icon">
             <LikeButton isLiked={isLiked} toggleLike={toggleLike} />
