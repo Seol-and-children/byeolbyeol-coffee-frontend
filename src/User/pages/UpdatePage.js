bc6c7713a4e21593b1af57c6b4df84335935cc8c
busker362 <busker362@gmial.com>
2023-12-20 05:21:47 +0900

    빠진 코드 다시 올립니다

--- src/User/pages/UpdatePage.js
+++ src/User/pages/UpdatePage.js
@@ -113,6 +113,7 @@ function UpdatePage(props) {
       const response = await axios.post('/users/checkUserPassword', { userId, userPassword });
       if (response.data === true) {
         setSuccessMessages(prev => ({ ...prev, currentPasswordSuccess: '현재 비밀번호와 일치합니다.' }));
+        setErrors(prevErrors => ({ ...prevErrors, currentPasswordError: '' }));
       } else {
         setErrors(prevErrors => ({ ...prevErrors, currentPasswordError: '현재 비밀번호와 일치하지 않습니다.' }));
         setSuccessMessages(prev => ({ ...prev, currentPasswordSuccess: '' }));
